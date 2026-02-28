c770643df42753047bde08adcd2176756812c380
Aman <dealstoreaman@gmail.com>
2026-02-28 12:55:42 +0530

    Updated Haryana-Section exam files, added manifest + service worker, and VSCode settings

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "akg-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/allimage/ApnaKnowledgeGroup.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
